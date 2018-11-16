7beab2d9bde2c43631f06123c6c38dff4f818322
BT_1N3_29 <jtan5662@bths.edu>
2018-11-16 14:46:24 -0500

    done done done done

--- src/HorseRunner.java
+++ src/HorseRunner.java
@@ -1,7 +1,7 @@
-public class horseRunner {
+public class HorseRunner {
         public static void main(String[] args) {
             Horse horse1= new Mustang("Secretariat", 12000);
-            Horse horse2= new Mustang("Dustry Trail", 22000);
+            Horse horse2= new Mustang("Dusty Trail", 22000);
             Horse horse3 = new Mustang("Silver", 12000);
 
             Horse[] horses = new Horse[6];
@@ -12,11 +12,11 @@ public class horseRunner {
             HorseBarn barn= new HorseBarn(horses);
 
             System.out.println(barn);
-            System.out.println("Dustry Trail is in space" + barn.findHorseSpace("Dusty Trail"));
+            System.out.println("Dusty Trail is in space: " + barn.findHorseSpace("Dusty Trail"));
 
             barn.consolidate();
 
             System.out.println(barn);
-            System.out.println("Dusty Trail is in now in space" + barn.findHorseSpace("Dusty Trail"));
+            System.out.println("Dusty Trail is in now in space: " + barn.findHorseSpace("Dusty Trail"));
         }
     }
